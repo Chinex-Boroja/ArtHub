770577467db65468fa58cf4eb081bb99e736675e
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2023-11-15 11:37:37 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ArtHub: 
+## Find an Article that interests you!
+### Enrich your body, soul and mind
---
+++ src/main/java/com/chinexboroja/secure_auth_system/model/User.java
@@ -0,0 +1,27 @@
+package com.chinexboroja.secure_auth_system.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(unique = true, nullable = false)
+    private String password;
+    private String roles;
+    private Date timestamp;
+}
